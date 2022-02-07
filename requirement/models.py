17d076424ccefaf03fcb64ba3578c1b5abbd8eb0
Ruhollah Ahmadi <roohix.code@gmail.com>
2022-02-07 23:01:11 +0430

    first release

--- requirement/models.py
+++ requirement/models.py
@@ -12,6 +12,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False, unique=True)
     email = db.Column(db.String(length=50), nullable=False, unique=True)
+    projects = db.relationship('Project', backref='owned_user', lazy=True)
 
     @property
     def password(self):
@@ -23,3 +24,31 @@ class User(db.Model, UserMixin):
 
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=100), nullable=False)
+    description = db.Column(db.String(length=1024))
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+    project = db.relationship('Requirement', backref='requirement_owner', lazy=True)
+
+
+class Requirement(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(length=400), nullable=False)
+    level = db.Column(db.Integer())
+    priority = db.Column(db.Integer())
+    req_type = db.Column(db.Integer())
+    changes = db.Column(db.Integer())
+    review = db.Column(db.Integer())
+    evaluation = db.Column(db.Integer())
+    evaluation_method = db.Column(db.Integer())
+    quality_factor = db.Column(db.Integer())
+    description = db.Column(db.String(length=1024))
+    project = db.Column(db.Integer(), db.ForeignKey('project.id'))
+
+    parent_id = db.Column(db.Integer, db.ForeignKey('requirement.id'))
+
+    parent = db.relationship('Requirement',remote_side = 'Requirement.id',back_populates = 'children', lazy=True)
+    children = db.relationship('Requirement',  back_populates = 'parent', lazy=True)
